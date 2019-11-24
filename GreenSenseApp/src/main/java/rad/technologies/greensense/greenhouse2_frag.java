99cceeb292efdcb608b10805ba1949b1b93b8d24
GreenSense <unconfigured@null.spigotmc.org>
2019-11-23 19:39:09 -0500

    Adding new files

---
+++ GreenSenseApp/src/main/java/rad/technologies/greensense/greenhouse2_frag.java
@@ -0,0 +1,85 @@
+package rad.technologies.greensense;
+
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+public class greenhouse2_frag extends Fragment {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_greenhouse2_frag, null);
+
+        flTempAndHumidity = root.findViewById(R.id.flTempAndHumidity);
+        flManualControl = root.findViewById(R.id.flManualControl);
+
+        tvTempAndHumidity = root.findViewById(R.id.tvTempAndHumidity);
+        tvManualControl = root.findViewById(R.id.tvManualControl);
+
+
+        flTempAndHumidity.setOnClickListener(this::onClick);
+        flManualControl.setOnClickListener(this::onClick);
+
+        return root;
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    FrameLayout flTempAndHumidity, flManualControl;
+    TextView tvTempAndHumidity, tvManualControl;
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+
+    public void onClick(View v) {
+
+        WhiteBackgroundFunction(flTempAndHumidity, tvTempAndHumidity);
+        WhiteBackgroundFunction(flManualControl, tvManualControl);
+
+
+        switch (v.getId()) {
+            case R.id.flTempAndHumidity:
+                GreenBackgroundFunction(flTempAndHumidity, tvTempAndHumidity);
+                startActivity(new Intent(getActivity(), TempAndHumidityActivity.class));
+                break;
+
+            case R.id.flManualControl:
+                GreenBackgroundFunction(flManualControl, tvManualControl);
+                startActivity(new Intent(getActivity(), DevicesActivity.class));
+
+                break;
+
+
+        }
+    }
+
+    private void WhiteBackgroundFunction(FrameLayout frameLayout, TextView textView) {
+        final int sdk = android.os.Build.VERSION.SDK_INT;
+        if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            frameLayout.setBackgroundDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.rounded_button_white));
+        } else {
+            frameLayout.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.rounded_button_white));
+        }
+
+        textView.setTextColor(getResources().getColor(R.color.green));
+
+    }
+
+    private void GreenBackgroundFunction(FrameLayout frameLayout, TextView textView) {
+
+        final int sdk = android.os.Build.VERSION.SDK_INT;
+        if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            frameLayout.setBackgroundDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.rounded_button_green));
+        } else {
+            frameLayout.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.rounded_button_green));
+        }
+        textView.setTextColor(getResources().getColor(R.color.white));
+
+
+    }
+}
---
+++ GreenSenseApp/src/main/java/rad/technologies/greensense/notes_frag.java
@@ -0,0 +1,94 @@
+package rad.technologies.greensense;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class notes_frag extends Fragment {
+    static ArrayList<String> notes = new ArrayList<>();
+    static ArrayAdapter arrayAdapter;
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_notes_frag, null);
+
+        SharedPreferences sharedPreferences = getActivity().getApplicationContext().getSharedPreferences("rad.technologies.greensense", Context.MODE_PRIVATE);
+        ListView listView = root.findViewById(R.id.listView);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
+
+        if (set == null) {
+            notes.add("Add Note");
+        }else {
+            notes = new ArrayList(set);
+        }
+        notes.add("Add Note");
+
+        arrayAdapter = new ArrayAdapter (getActivity(),android.R.layout.simple_list_item_1, notes);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity().getBaseContext(), NoteEditorActivity.class);
+                intent.putExtra("noteId", i);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it=new Intent(getActivity(), NoteEditorActivity.class);
+                startActivity(it);
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
+
+                final int itemToDelete = i;
+
+                new AlertDialog.Builder(getActivity())
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+
+                                SharedPreferences sharedPreferences = getActivity().getApplicationContext().getSharedPreferences("rad.technologies.greensense", Context.MODE_PRIVATE);
+
+                                HashSet<String> set =new HashSet(NotesActivity.notes);
+
+                                sharedPreferences.edit().putStringSet("notes",set).apply();
+
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+                return true;
+            }
+        });
+        return root;
+    }
+}
